a1c5d52806143ee5b36ab00b3040a032835e2924
GuilhermePerina <guilherme.perina55@gmail.com>
2024-07-11 20:51:51 -0300

    update

---
+++ ex009.py
@@ -0,0 +1,3 @@
+n1=int(input('Escreva um número '))
+print('Seu sucessor é {}'.format(n1+1))
+print('Seu antecessor é', n1-1)
---
+++ ex012.py
@@ -0,0 +1,5 @@
+v1=float(input('Qual a metragem? '))
+v2=(v1*100)
+v3=(v2*100)
+
+print('Serão {} centímetros e {} milímetros'.format(round(v2), round(v3)))
