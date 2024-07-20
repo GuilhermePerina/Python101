832a708e42a81bd8e82d914870403fb24aabaef9
GuilhermePerina <guilherme.perina55@gmail.com>
2024-07-19 22:33:12 -0300

    update

---
+++ extra01.py
@@ -0,0 +1,22 @@
+
+n1 = int(input('Digite um número: '))
+
+pares = []
+impares = []
+
+for count in range(n1):
+    if(count%2==0):
+        pares.append(count)
+    else:
+        impares.append(count)
+
+opcao = int(input('\nVocê quer ver os números impares ou pares de 0 até {}? Digite 1 para impar e 2 para par: '.format(n1)))
+
+print('\n')
+if opcao == 1:
+    print(impares)
+
+if opcao == 2:
+    print(pares)
+    
+
