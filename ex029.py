4fa0b08252cc5b441864a3d79e520d3a7a754397
GuilhermePerina <guilherme.perina55@gmail.com>
2024-07-25 21:33:55 -0300

    update

--- ex029.py
+++ ex029.py
@@ -1,8 +1,10 @@
-nome=input('Me diga seu nome: \n')
+nome=str(input('Me diga seu nome: \n')).strip()
 
 print(nome.upper(), ': com todas as letras Maiúsculas')
 print(nome.lower(), ': com todas as letras Minúsculas')
 print(nome, ': tem {} caracteres ao todo'.format(len(nome)))
+print(nome, ': tem {} letras ao todo, sem considerar os espaços'.format(len(nome) - nome.count(' ')))
+print(nome, ': seu primeiro nome tem {} letras'.format(nome.find(' ')))
 
 nome2=nome.split()
 nome3=''.join(nome2)
