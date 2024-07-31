bfbe8610c4f07ef98bb0a2d057cf384f35f56de2
GuilhermePerina <guilherme.perina55@gmail.com>
2024-07-30 22:58:51 -0300

    update

--- ex033.py
+++ ex033.py
@@ -3,3 +3,4 @@ frase=str(input('Digite uma frase: ')).strip()
 print('A letra "A" aparece {} vezes na frase.'.format(frase.count('a') + frase.count('A')))
 print('A primeira letra "A" apareceu na posição {}'.format(frase.upper().find('A')))
 print('A última letra "A" apareceu na posição {}'.format(frase.lower().rfind('a')))
+#
