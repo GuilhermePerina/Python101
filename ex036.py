ec18512545a9e53adf1541710e53f451bd6959a1
GuilhermePerina <guilherme.perina55@gmail.com>
2024-07-31 22:00:44 -0300

    update

--- ex036.py
+++ ex036.py
@@ -1,3 +1,14 @@
 #Exercício Python 029: Escreva um programa que leia a velocidade de um carro. 
 # Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. 
 # A multa vai custar R$7,00 por cada Km acima do limite.
+
+
+vel=int(input('Digite a velocidade do carro: '))
+
+multa=float(vel*7)
+
+if vel>80:
+    print('Você foi multado em R$ {} por excesso de velocidade! Dirija com cuidado!'.format(multa))
+
+else:
+    print('Parabéns! Continue andando na velocidade permitida pela via')
