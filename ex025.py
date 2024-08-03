f067d257ab5fdf584d9180bbbfed17829548ae7a
GuilhermePerina <guilherme.perina55@gmail.com>
2024-08-03 19:33:24 -0300

    update

--- ex025.py
+++ ex025.py
@@ -1,3 +1,5 @@
+# Exercício Python 020: O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
 import random
 name=input('Qual é o 1º aluno? ')
 name1=input('Qual é o 2º aluno? ')
--- ex026.py
+++ ex026.py
@@ -1,2 +1,4 @@
+# Exercício Python 021: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
 import python_play.player
 python_play.player.play_it('0.mp3')
