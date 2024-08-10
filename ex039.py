bd984205acfe5710e5cd1686fe7ae919a50e9db7
GuilhermePerina <guilherme.perina55@gmail.com>
2024-08-10 12:58:59 -0300

    commit

--- ex039.py
+++ ex039.py
@@ -11,9 +11,9 @@ if ano == 0:
     ano = date.today().year
 
 if bissexto==0 and ano % 100 != 0 or ano%400==0:
-    print('O ano {} é Bissexto'.format(ano))
+    print('O ano {} é Bissexto.'.format(ano))
 else:
-    print('O ano {} não é Bissexto'.format(ano))
+    print('O ano {} não é Bissexto.'.format(ano))
 
 #or
 
