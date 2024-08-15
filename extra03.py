037df78bfdee8cf160b07fe23313624ebdcd191a
GuilhermePerina <guilherme.perina55@gmail.com>
2024-08-14 23:13:45 -0300

    update

--- extra03.py
+++ extra03.py
@@ -3,26 +3,23 @@ import random
 from time import sleep
 
 print("\nVamos jogar joken-po?\n")
-
 userOption = str(input("Faça sua escolha: ")).strip().lower()
 
-print("Processando...")
 sleep(1)
+print("Processando...")
 print('\n')
 sleep(2)
 options=['pedra', 'papel', 'tesoura']
 pcOption= random.choice(options)
 
 print('COMPUTADOR           VOCÊ')
+print(f'  {pcOption}      x      {userOption}\n')
 
 if pcOption == userOption:
-    print(f'  {pcOption}      x      {userOption}\n')
     print('         Empatamos!')
 elif userOption=='pedra' and pcOption=='tesoura' or userOption=='papel' and pcOption=='pedra' or userOption=='tesoura' and pcOption=='papel':
-    print(f'  {pcOption}      x      {userOption}\n')
     print('         Você ganhou!')
 else:
-    print(f'  {pcOption}      x      {userOption}\n')
     print('         Eu ganhei!')
 
 print('\n\n--------fim-------\n\n')
