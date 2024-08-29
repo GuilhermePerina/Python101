97359ebf3268b452e026b0564b835f032f5e0cb0
GuilhermePerina <guilherme.perina55@gmail.com>
2024-08-28 21:46:51 -0300

    update

--- ex061.py
+++ ex061.py
@@ -1,3 +1,26 @@
 #Exercício Python 051: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
 
+print('--'*20)
+print('10 Termos de uma PA')
+print('--'*20)
 
+termo=int(input('Primeiro Termo: '))
+razao=int(input('Razão: '))
+primeiro=termo+razao
+proximo=termo
+
+print(termo, end=' > ')
+for n in range (1, 10):
+    proximo+=razao
+    print(proximo, end=' > ')
+print('Acabou\n')
+
+#or
+
+primeiro=int(input('Primeiro Termo: '))
+razao1=int(input('Razão: '))
+decimo=primeiro+(10-1)*razao1
+
+for c in range (primeiro, decimo + razao1, razao1):
+    print(c, end=' > ')
+print('Acabou')
---
+++ ex062.py
@@ -0,0 +1,18 @@
+# Exercício Python 052: Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+# (é divisível apenas por um e por ele mesmo)
+
+n=int(input('Digite um número: '))
+count=0
+for i in range(1, n+1):
+    #print(i, end=' ')
+    if n%i==0:
+        count+=1
+        print(i, end=' ')
+print('')
+if n==1:
+    print('O número {} NÃO é primo'.format(n))
+elif count<=2:
+    print('O número {} é primo'.format(n))
+else:
+    print('O número {} NÃO é primo'.format(n))
+print('')
