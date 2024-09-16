39b4ddc1d5248d663113da216343acb739999d2a
GuilhermePerina <guilherme.perina55@gmail.com>
2024-09-15 21:56:17 -0300

    update

--- ex065.py
+++ ex065.py
@@ -14,9 +14,9 @@ for pessoa in range (1, 6):
         if peso<menor:
             menor=peso
 
-print('')
+print(' ')
 print('O maior peso foi {} kg e o menor peso foi {} kg'.format(maior, menor))
-print('')
+print(' ')
 
 # or
 
