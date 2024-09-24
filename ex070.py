b142f6e28597e44f6356535de61804a5735a194f
GuilhermePerina <guilherme.perina55@gmail.com>
2024-09-24 20:44:51 -0300

    update

--- ex070.py
+++ ex070.py
@@ -1,14 +1,28 @@
 #Exercício Python 060: Faça um programa que leia um número qualquer e mostre o seu fatorial.
 
 n=int(input('Digite um número para calcular seu fatorial: '))
-fat=0
-mult=0
-for n1 in range (n, 1, -1):
-    fat=fat*(n1*(n1-1))
-    mult=mult*(n1-1)
-    print(n1)
-    print(fat)
-    print(mult)
-    print('===')
-#print('Calculando...')
-#print('O fatorial de {} = {}'.format(n, fat))
+n1=n
+f=1
+for n1 in range (n, 0, -1):
+    f*=n1
+print('Calculando...')
+print('O fatorial de {} = {}'.format(n, f))
+
+#or
+
+from math import factorial
+n=int(input('Digite um número para calcular seu fatorial: '))
+f=factorial(n)
+print('O fatorial de {} é {}'.format(n, f))
+
+#or
+
+n=int(input('Digite um número para calcular seu fatorial: '))
+c = n
+f = 1
+while c>0:
+    print ('{}'.format(c), end='')
+    print (' x ' if c> 1 else ' = ', end='')
+    f*=c
+    c-=1
+print('{}'.format(f))
