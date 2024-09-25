cceb423617c92696a6f72d7ac17d2de7f70fd603
GuilhermePerina <guilherme.perina55@gmail.com>
2024-09-25 20:59:41 -0300

    update

--- ex071.py
+++ ex071.py
@@ -5,5 +5,29 @@ print ('''Gerador de P.A.
 
 n=int(input('Primeiro termo: '))
 razao=int(input('Razão da P.A.: '))
+count = 0
+primeiro=n+razao
+proximo=n
 
-print()
+print(n, end=' > ')
+
+while count < 9:
+    count+=1
+    proximo+=razao
+    print(proximo, end=' > ')
+    if count>8:
+        print('FIM')    
+
+
+#or
+
+primeiro=int(input('Primeiro termo: '))
+razao=int(input('Razão: '))
+termo = primeiro
+count = 1
+
+while count <= 10:
+    print('{} > '.format(termo), end='')
+    termo+=razao
+    count+=1
+print('FIM')
